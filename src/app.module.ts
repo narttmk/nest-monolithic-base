03e8ab79413d491ff47c738b7db7eb668e288e10
Ethan Tran <tvtung@marusysvina.com>
2025-06-18 17:44:31 +0700

    update k8s services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 import { loggerConfig } from './config/logger.config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Keyv } from 'keyv';
+import { createKeyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
   imports: [
@@ -18,6 +24,23 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
     WinstonModule.forRoot(loggerConfig),
     UsersModule,
     AuthModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: ((configService: ConfigService) => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+            }),
+            createKeyv(
+              `redis://${configService.get<string>('REDIS_HOST')}:${configService.get<string>('REDIS_PORT')}`,
+            ),
+          ],
+        };
+      }) as any,
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [
